de65f2016e085de9b778b2caeb4293c7c1cf1163
diotriekaputra <diotriekaputra97@gmail.com>
2021-10-15 19:04:39 +0700

    Authentication
    OOP

---
+++ Autentication/Autentication/Login.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Authentication
+{
+    class Login : User
+    {
+        public static void LoginUser()
+        {
+            try
+            {
+                var index = data.FindIndex(c => c.userNames == Username);
+                var id = data[index].userIds;
+                var idx = data.FindIndex(c => c.userIds == id);
+                if (data[idx].userNames == Username)
+                {
+                    bool cek = BCrypt.Net.BCrypt.Verify(Password, data[idx].passwords);
+                    if (cek == true)
+                    {
+                        Console.WriteLine("login success");
+                        Program.MenuUser(Username);
+                    }
+                    else
+                    {
+                        Console.WriteLine("GAGAL LOGIN PASSWORD SALAH !!!");
+                        kon = Console.ReadLine();
+                        Program.Main();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("GAGAL LOGIN USERNAME SALAH !!!");
+                kon = Console.ReadLine();
+                Program.Main();
+            }
+        }
+        public static void InfoUser(string usernames)
+        {
+            Console.Clear();
+            Console.WriteLine("======================");
+            int i = data.FindIndex(c => c.userNames == usernames);
+            if (usernames == data[i].userNames)
+            {
+                Console.WriteLine($"ID      : { data[i].userIds}");
+                Console.WriteLine("NAME     :" + data[i].firstNames + " " + data[i].lastNames);
+                Console.WriteLine("USERNAME :" + data[i].userNames);
+                Console.WriteLine("PASSWORD :" + data[i].passwords);
+            }
+            Console.WriteLine("======================");
+            Console.WriteLine("ENTER UNTUK KEMBALI !!!");
+            kon = Console.ReadLine();
+            Program.MenuUser(usernames);
+        }
+        public static void HapusUser(string hpsUser)
+        {
+            Console.Clear();
+            Console.Write($"Apakah ada yakin ingin menghapus ID ({hpsUser}) ? (Y|N)");
+            string konfirmasi = Console.ReadLine();
+            if ((konfirmasi == "Y") || (konfirmasi == "y"))
+            {
+                int i = data.FindIndex(c => c.userNames == hpsUser);
+                data.RemoveAt(i);
+                Console.Clear();
+                Console.WriteLine("========== HAPUS DATA Berhasil ==========");
+                Console.WriteLine("========== SILAHKAN MASUK KE MENU UTAMA ==========");
+                Console.WriteLine("Tekan ENTER untuk Kembali");
+                string con = Console.ReadLine();
+                Program.Main();
+            }
+            else if ((konfirmasi == "N") || (konfirmasi == "n"))
+            {
+                Program.MenuUser(hpsUser);
+            }
+        }
+        public static void EditUser(string edtUser, int ubah)
+        {
+            Console.Clear();
+            try
+            {
+                var hasNumber = new Regex(@"[0-9]+");
+                var hasUpperChar = new Regex(@"[A-Z]+");
+                var hasMinimum8Chars = new Regex(@".{6,}");
+                string userNameBaru, konfirmasi, konfirmasiPassword;
+
+                int i = data.FindIndex(c => c.userNames == edtUser);
+                bool cek;
+
+                if (ubah == 1)
+                {
+                    Console.WriteLine("======= UBAH USER =======");
+                    Console.Write("USERNAME BARU            : ");
+                    userNameBaru = Console.ReadLine();
+
+                    Console.Write($"Apakah ada yakin ingin mengubah ID ({edtUser}) ? (Y|N)");
+                    konfirmasi = Console.ReadLine();
+                    if ((konfirmasi == "Y") || (konfirmasi == "y"))
+                    {
+                        Console.WriteLine("Masukkan Password untuk mengubah Username ");
+                        Console.Write("Password : ");
+                        konfirmasiPassword = Console.ReadLine();
+                        cek = BCrypt.Net.BCrypt.Verify(konfirmasiPassword, data[i].passwords);
+                        if (cek == true)
+                        {
+                            int j = 0;
+                            for (int a = 0; a < data.Count; a++)
+                            {
+                                if (userNameBaru == data[a].userNames)
+                                {
+                                    j = 1;
+                                }
+                            }
+                            if (j > 0)
+                            {
+                                Console.WriteLine("USERNAME SUDAH ADA !!!");
+                                kon = Console.ReadLine();
+                                Program.MenuUser(edtUser);
+                            }
+                            else
+                            {
+                                data[i].userNames = userNameBaru;
+                                Console.WriteLine("======= BERHASIL SILAHKAN LOGIN KEMBALI =======");
+                                kon = Console.ReadLine();
+                                Program.Main();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("======= PASSWORD SALAH !!! =======");
+                            kon = Console.ReadLine();
+                            Program.MenuUser(edtUser);
+                        }
+                    }
+                    else if ((konfirmasi == "N") || (konfirmasi == "n"))
+                    {
+                        Program.MenuUser(edtUser);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("======= UBAH PASSWORD USER =======");
+                    Console.Write("PASSWORD BARU                : ");
+                    string passBaru = Console.ReadLine();
+                    Console.Write("PASSWORD KONFIRMASI BARU     : ");
+                    string passBaruKonfir = Console.ReadLine();
+                    if (passBaru == "")
+                    {
+                        Console.WriteLine("Password Should not be empty");
+                        kon = Console.ReadLine();
+                        Program.MenuUser(edtUser);
+                    }
+                    else if (!hasNumber.IsMatch(passBaru))
+                    {
+                        Console.WriteLine("Password should contains number");
+                        kon = Console.ReadLine();
+                        Program.MenuUser(edtUser);
+                    }
+                    else if (!hasUpperChar.IsMatch(passBaru))
+                    {
+                        Console.WriteLine("Password should has Upper Character");
+                        kon = Console.ReadLine();
+                        Program.MenuUser(edtUser);
+                    }
+                    else if (!hasMinimum8Chars.IsMatch(passBaru))
+                    {
+                        Console.WriteLine("Password should has Minimum 6 Characters");
+                        kon = Console.ReadLine();
+                        Program.MenuUser(edtUser);
+                    }
+                    else
+                    {
+                        if (passBaru == passBaruKonfir)
+                        {
+                            Console.Write($"Apakah ada yakin ingin mengubah Password ID ({edtUser}) ? (Y|N)");
+                            konfirmasi = Console.ReadLine();
+                            if ((konfirmasi == "Y") || (konfirmasi == "y"))
+                            {
+                                Console.WriteLine("Masukkan Password Lama untuk mengubah Password ");
+                                Console.Write("Password : ");
+                                konfirmasiPassword = Console.ReadLine();
+                                cek = BCrypt.Net.BCrypt.Verify(konfirmasiPassword, data[i].passwords);
+                                if (cek == true)
+                                {
+                                    hashPassword = BCrypt.Net.BCrypt.HashString(passBaru);
+                                    data[i].passwords = hashPassword;
+                                    Console.WriteLine("======= BERHASIL SILAHKAN LOGIN KEMBALI =======");
+                                    kon = Console.ReadLine();
+                                    Program.Main();
+                                }
+                                else
+                                {
+                                    Console.WriteLine("======= PASSWORD SALAH !!! =======");
+                                    kon = Console.ReadLine();
+                                    Program.MenuUser(edtUser);
+                                }
+                            }
+                            else if ((konfirmasi == "N") || (konfirmasi == "n"))
+                            {
+                                Program.MenuUser(edtUser);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("======= PASSWORD TIDAK SAMA !!! =======");
+                            kon = Console.ReadLine();
+                            Program.MenuUser(edtUser);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR INPUT !!!!");
+                kon = Console.ReadLine();
+                Program.MenuUser(edtUser);
+            }
+        }
+    }
+}
---
+++ Autentication/Autentication/User.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Authentication
+{
+    class User
+    {
+        public static string Username { get; set; }
+        public static string Password { get; set; }
+        public static string KonfirmasiPassword { get; set; }
+        public static string FirstName { get; set; }
+        public static string LastName { get; set; }
+
+        protected static List<User> data = new List<User>();
+
+        public string firstNames, lastNames, passwords, userNames, userIds;
+        public static string idUser, kon, hashPassword;
+        public static int id = 0;
+
+        public static void TambahUser()
+        {
+            try
+            {
+                if (KonfirmasiPassword == Password)
+                {
+                    var hasNumber = new Regex(@"[0-9]+");
+                    var hasUpperChar = new Regex(@"[A-Z]+");
+                    var hasMinimum8Chars = new Regex(@".{6,}");
+                    if (Password == "")
+                    {
+                        Console.WriteLine("Password Should not be empty");
+                        kon = Console.ReadLine();
+                        Program.CreateUser();
+                    }
+                    else if (!hasNumber.IsMatch(Password))
+                    {
+                        Console.WriteLine("Password should contains number");
+                        kon = Console.ReadLine();
+                        Program.CreateUser();
+                    }
+                    else if (!hasUpperChar.IsMatch(Password))
+                    {
+                        Console.WriteLine("Password should has Upper Character");
+                        kon = Console.ReadLine();
+                        Program.CreateUser();
+                    }
+                    else if (!hasMinimum8Chars.IsMatch(Password))
+                    {
+                        Console.WriteLine("Password should has Minimum 6 Characters");
+                        kon = Console.ReadLine();
+                        Program.CreateUser();
+                    }
+                    else
+                    {
+
+                        idUser = (Convert.ToString((data.Count) + 1));
+                        Username = FirstName.Substring(0, 2) + LastName.Substring(0, 2) + idUser;
+                        hashPassword = BCrypt.Net.BCrypt.HashString(Password);
+
+                        data.Add(new User { userIds = idUser, firstNames = FirstName, lastNames = LastName, passwords = hashPassword, userNames = Username });
+                        Console.WriteLine("======= BERHASIL =======");
+                        kon = Console.ReadLine();
+                        Program.Main();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Password dan konfirmasi password tidak sama !!!");
+                    kon = Console.ReadLine();
+                    Program.CreateUser();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("ERROR INPUT !!!!");
+                kon = Console.ReadLine();
+                Program.Main();
+            }
+        }
+        public static void ShowUser(string username)
+        {
+            Console.Clear();
+            if (data.Count == 0)
+            {
+                Console.WriteLine("======Show User=======");
+                Console.WriteLine("DATA BELUM ADA");
+                kon = Console.ReadLine();
+                Program.Main();
+            }
+            else
+            {
+                if (username == "")
+                {
+                    Console.WriteLine("======Show User=======");
+                    Console.WriteLine("======================");
+                    for (int i = 0; i < data.Count; i++)
+                    {
+                        Console.WriteLine($"ID USER  : {data[i].userIds}");
+                        Console.WriteLine($"NAME     : {data[i].firstNames} {data[i].lastNames}");
+                        Console.WriteLine($"USERNAME : {data[i].userNames}");
+                        Console.WriteLine($"PASSWORD : {data[i].passwords}");
+                        Console.WriteLine("======================");
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        int i = data.FindIndex(c => c.userNames == username);
+                        if (username == data[i].userNames)
+                        {
+                            Console.WriteLine($"ID      : { data[i].userIds}");
+                            Console.WriteLine("NAME     :" + data[i].firstNames + " " + data[i].lastNames);
+                            Console.WriteLine("USERNAME :" + data[i].userNames);
+                            Console.WriteLine("PASSWORD :" + data[i].passwords);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("DATA TIDAK ADA !!!");
+                        kon = Console.ReadLine();
+                        Program.Main();
+                    }
+                }
+                Console.WriteLine("ENTER UNTUK KEMBALI !!!");
+                kon = Console.ReadLine();
+                Program.Main();
+            }
+        }
+    }
+}
